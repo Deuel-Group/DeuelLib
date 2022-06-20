ae487163c4cc1fea811ae3b5fb3c457440e4d30f
James Gavin <jogavin@wisc.edu>
2022-06-20 01:24:09 -0500

    Initial commit

---
+++ src/main/java/com/jmsgvn/DeuelLib.java
@@ -0,0 +1,16 @@
+package com.jmsgvn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeuelLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
